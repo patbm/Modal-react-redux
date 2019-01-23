c073faddafd6ba54025f6df0ea61d887af410e82
Patricia Barcenilla <patricia.barcenilla@softtek.com>
2019-01-23 15:50:16 +0100

    Updates

---
+++ precache-manifest.6240a4c6729ea397affa032e7f3efe94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f81c9ae7786b01700e8d",
+    "url": "/modal-react-redux/static/js/runtime~main.f81c9ae7.js"
+  },
+  {
+    "revision": "f23dc301aed33f9b1508",
+    "url": "/modal-react-redux/static/js/main.f23dc301.chunk.js"
+  },
+  {
+    "revision": "be18b2b3c342e16faa37",
+    "url": "/modal-react-redux/static/js/1.be18b2b3.chunk.js"
+  },
+  {
+    "revision": "f23dc301aed33f9b1508",
+    "url": "/modal-react-redux/static/css/main.d80acc4f.chunk.css"
+  },
+  {
+    "revision": "9359129fcf69c83770bc2d0e95c0630f",
+    "url": "/modal-react-redux/index.html"
+  }
+];
